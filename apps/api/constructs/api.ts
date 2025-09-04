87386fe2bbf6e072f959c5771158a0dc66b6448e
Erik van Dam <erikvandam93@gmail.com>
2025-09-04 11:57:46 +0200

    feat: added monitoring to the api service

---
+++ apps/api/constructs/api.ts
@@ -0,0 +1,76 @@
+import { Construct } from 'constructs'
+import { HonoRestApi } from '@internal/cdk-utils/hono-rest-api'
+import { Cors } from 'aws-cdk-lib/aws-apigateway'
+import { RegionStack } from '@internal/cdk-utils/region-stack'
+import type { IEventBus } from 'aws-cdk-lib/aws-events'
+import { Duration } from 'aws-cdk-lib'
+
+interface Props {
+  databaseUrl: string
+  domainName: string
+  eventBus: IEventBus
+}
+
+export class Api extends Construct {
+  constructor(scope: Construct, id: string, props: Props) {
+    super(scope, id)
+
+    const stack = RegionStack.getStack(this)
+
+    const hostedZone = stack.getDelegatedHostedZone(props.domainName)
+
+    const { handler, api } = new HonoRestApi(this, 'api', {
+      domainName: props.domainName,
+      handlerProps: {
+        entry: './src/index.ts',
+        environment: {
+          DATABASE_URL: props.databaseUrl,
+          EVENT_BUS_NAME: props.eventBus.eventBusName,
+        },
+      },
+      hostedZone,
+      restApiProps: {
+        defaultCorsPreflightOptions: {
+          allowCredentials: true,
+          allowHeaders: [
+            'Accept',
+            'Authorization',
+            'Origin',
+            'X-Requested-With',
+            'Content-Type',
+          ],
+          allowMethods: Cors.ALL_METHODS,
+          allowOrigins: Cors.ALL_ORIGINS,
+        },
+      },
+    })
+
+    props.eventBus.grantPutEventsTo(handler)
+
+    stack.monitoring.addLargeHeader('Atlas API')
+      .monitorApiGateway({
+        api,
+        addLatencyP95Alarm: {
+          Warning: {
+            maxLatency: Duration.seconds(10),
+            datapointsToAlarm: 1,
+          },
+        },
+        add5XXFaultCountAlarm: {
+          Warning: {
+            maxErrorCount: 5,
+            datapointsToAlarm: 1,
+          },
+        },
+        add4XXErrorCountAlarm: {
+          Warning: {
+            maxErrorCount: 5,
+            datapointsToAlarm: 1,
+          },
+        },
+      })
+      .monitorLambdaFunction({
+        lambdaFunction: handler,
+      })
+  }
+}
--- apps/api/lib/index.ts
+++ apps/api/lib/index.ts
@@ -1,10 +1,9 @@
 import type { RegionStackProps } from '@internal/cdk-utils/region-stack'
 import { RegionStack } from '@internal/cdk-utils/region-stack'
 import type { Construct } from 'constructs'
-import { HonoRestApi } from '@internal/cdk-utils/hono-rest-api'
 import { StringParameter } from 'aws-cdk-lib/aws-ssm'
 import { EventBus } from 'aws-cdk-lib/aws-events'
-import { Cors } from 'aws-cdk-lib/aws-apigateway'
+import { Api } from '../constructs/api'
 
 interface Props extends RegionStackProps {
   databaseUrl: string
@@ -15,8 +14,6 @@ export class ApiService extends RegionStack {
   constructor(scope: Construct, id: string, props: Props) {
     super(scope, id, props)
 
-    const hostedZone = this.getDelegatedHostedZone(props.domainName)
-
     const eventBusArn = StringParameter.fromStringParameterName(
       this,
       'event-bus-arn',
@@ -25,32 +22,10 @@ export class ApiService extends RegionStack {
 
     const eventBus = EventBus.fromEventBusArn(this, 'event-bus', eventBusArn)
 
-    const { handler } = new HonoRestApi(this, 'api', {
+    new Api(this, 'api', {
+      databaseUrl: props.databaseUrl,
       domainName: props.domainName,
-      handlerProps: {
-        entry: './src/index.ts',
-        environment: {
-          DATABASE_URL: props.databaseUrl,
-          EVENT_BUS_NAME: eventBus.eventBusName,
-        },
-      },
-      hostedZone,
-      restApiProps: {
-        defaultCorsPreflightOptions: {
-          allowCredentials: true,
-          allowMethods: Cors.ALL_METHODS,
-          allowHeaders: [
-            'Accept',
-            'Authorization',
-            'Origin',
-            'X-Requested-With',
-            'Content-Type',
-          ],
-          allowOrigins: Cors.ALL_ORIGINS,
-        },
-      },
+      eventBus,
     })
-
-    eventBus.grantPutEventsTo(handler)
   }
 }
