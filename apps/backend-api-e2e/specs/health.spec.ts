0f4bf4c14ab1f6dc35a468623ba82cde123b6aff
Erik van Dam <erikvandam93@gmail.com>
2025-09-10 17:53:34 +0200

    [skip ci] refactor: renamed specs

--- apps/backend-api-e2e/specs/health.spec.ts
+++ apps/backend-api-e2e/specs/health.spec.ts
@@ -1,6 +1,6 @@
 import test, { expect } from '@playwright/test'
 
-test('app is healthy', async ({ request }) => {
+test('api is healthy', async ({ request }) => {
   const response = await request.get('/health')
 
   expect(response.status()).toBe(200)
